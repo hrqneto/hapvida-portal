daac585063f82c241421aa4ed7e7998922c12180
Henrique <hrqneto@gmail.com>
2024-12-06 17:54:57 -0300

    feat: setup configurado - projeto portalHapvida

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // Verifica o HTML e todos os arquivos em `src`
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
